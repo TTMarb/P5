e3b48cf02d4e81cf35f425e3f7fa28440718e32c
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-10-17 14:09:37 +0200

    Added print statement

--- Onboard-SDK/sample/platform/linux/teeeeest/test_project.cpp
+++ Onboard-SDK/sample/platform/linux/teeeeest/test_project.cpp
@@ -137,9 +137,10 @@ getGPS(DJI::OSDK::Vehicle* vehicle)
 
     std::cout << "Counter = " << elapsedTimeInMs << ":\n";
     std::cout << "-------\n";
-    std::cout << "Get fucked";
-    // std::cout << "GPS state           (time, long, lat)     = " << gps.time
-    //           << ", " << gps.longitude << ", " << gps.latitude << "\n";
+    std::cout << "GPS state           (time, long, lat)     = " << gps.time
+              << "\n";
+    //           << ", " << gps.longitude << ", " << gps.latitude
+    //           << "\n";
     std::cout << "-------\n\n";
 
     usleep(500000);
