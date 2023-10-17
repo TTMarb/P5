328ffb30c33c304a510b2dcad5606001a229106b
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-10-17 13:34:10 +0200

    removed error

--- Onboard-SDK/sample/platform/linux/teeeeest/test_project.cpp
+++ Onboard-SDK/sample/platform/linux/teeeeest/test_project.cpp
@@ -137,8 +137,9 @@ getGPS(DJI::OSDK::Vehicle* vehicle)
 
     std::cout << "Counter = " << elapsedTimeInMs << ":\n";
     std::cout << "-------\n";
-    std::cout << "RC Commands           (time, long, lat)     = " << gps.time
-              << ", " << gps.longitude << ", " << gps.latitude << "\n";
+    std::cout << "Get fucked";
+    // std::cout << "GPS state           (time, long, lat)     = " << gps.time
+    //           << ", " << gps.longitude << ", " << gps.latitude << "\n";
     std::cout << "-------\n\n";
 
     msleep(500);
