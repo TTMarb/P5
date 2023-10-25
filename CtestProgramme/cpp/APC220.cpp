c67f5669b0c8ea03fc9b289f0e33175633bbc380
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-10-25 15:47:09 +0200

    Added debug step

--- CtestProgramme/cpp/APC220.cpp
+++ CtestProgramme/cpp/APC220.cpp
@@ -110,7 +110,7 @@ bool APC220::read2radio(int serial_port, char* outputarray, unsigned int outputL
     }
     if (true == msggood) {
         std::cout << "Received: " << buffer << std::endl;
-        str strcpy(outputarray, buffer);
+        strcpy(outputarray, buffer, length);
         return true;
     } else {
         return false;
