35124b887c1842eed1f5dd054189c2b166654f6d
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-10-30 11:50:38 +0100

    Can't do math 4 shit

--- Onboard-SDK/project_folder/rotation_test/rotation.cpp
+++ Onboard-SDK/project_folder/rotation_test/rotation.cpp
@@ -61,9 +61,9 @@ void getRotation(Vehicle* vehicle) {
         std::cout << "Angular Rate in z direction:          = " << velocity.z << "\n";
         std::cout << "Magnetometer  (x,y,z)                 = " << magnet.x << ", " << magnet.y << ", " << magnet.z
                   << "\n";
-        std::cout << "1 = " << sqrt(pow(abs(sin(magnet.x / 1500.0)), 2) + pow(abs(cos(magnet.y / 1500.0)), 2))
-                  << ", sin(x/1500) = " << sin(magnet.x / 1500.0) << ", cos(y/1500) = " << cos(magnet.y / 1500.0)
-                  << "\n";
+        std::cout < < < < asin(magnet.x / 1500.0) + acos(magnet.y / 1500.0)
+                              << ", asin(x/1500) = " << sin(magnet.x / 1500.0)
+                              << ", acos(y/1500) = " << cos(magnet.y / 1500.0) << "\n";
         std::cout << "-------\n";
 
         sleep(1);
