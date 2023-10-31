64b67e79bb43b35b2bb8b429fa21563d907d8650
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-10-31 15:09:56 +0100

    Restructured code

--- Onboard-SDK/project_folder/rotation_test/rotation.cpp
+++ Onboard-SDK/project_folder/rotation_test/rotation.cpp
@@ -110,7 +110,7 @@ void getRotation(Vehicle* vehicle) {
     sleep(2);
 
     float32_t degree;
-    float32_t degTarget = degStart - 45;
+    float32_t degTarget = degStart - 90;
     printf("degStart: %f, degTarget: %f\n", degStart, degTarget);
 
     int time = 0;
