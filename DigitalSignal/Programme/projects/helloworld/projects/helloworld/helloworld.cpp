e2eee4a3dabfc7cbba59b1357d01346e62f53532
Lukas-Fasken <56631858+Lukas-Fasken@users.noreply.github.com>
2023-10-31 12:11:50 +0100

    something

--- DigitalSignal/Programme/projects/helloworld/projects/helloworld/helloworld.cpp
+++ DigitalSignal/Programme/projects/helloworld/projects/helloworld/helloworld.cpp
@@ -6,12 +6,6 @@ using namespace std;
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
     
-    cout << endl;
+    cout << 'helloworld';
 }
