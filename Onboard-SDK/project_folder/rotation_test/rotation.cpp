375bc9c8d89e9dfbe3a5a52a440643f34dc91624
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-10-31 13:09:00 +0100

    Fixing angle calculations

--- Onboard-SDK/project_folder/rotation_test/rotation.cpp
+++ Onboard-SDK/project_folder/rotation_test/rotation.cpp
@@ -46,11 +46,12 @@ void getRotation(Vehicle* vehicle) {
 
     const int TIMEOUT = 20;
     double yawInRad;
-    uint8_t dataFreq = 7;
+
+    DJI::OSDK::FREQ test = DJI::OSDK::FREQ_400HZ;
 
     // Re-set Broadcast frequencies to their default values
     ACK::ErrorCode ack = vehicle->broadcast->setBroadcastFreqDefaults(TIMEOUT);
-    ACK::ErrorCode wack = vehicle->broadcast->setBroadcastFreq(&dataFreq, TIMEOUT);
+    ACK::ErrorCode wack = vehicle->broadcast->setBroadcastFreq(test, TIMEOUT);
     magnet = vehicle->broadcast->getMag();
 
     // Print in a loop for 2 seconds
