aa4eb6c03ca91f6cdd94abd6a400827c46d414e4
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-10-31 14:06:59 +0100

    Testing Yaw control

--- Onboard-SDK/project_folder/rotation_test/rotation.cpp
+++ Onboard-SDK/project_folder/rotation_test/rotation.cpp
@@ -109,7 +109,7 @@ void getRotation(Vehicle* vehicle) {
         std::cout << "\t Deg START: " << degStart << " :)\n";
     }
 
-    vehicle->control->positionAndYawCtrl(0, 0, 3, 90); //1.57); //yaw + 90.0);
+    vehicle->control->positionAndYawCtrl(0, 0, 3, 10); //1.57); //yaw + 90.0);
     while (1) {
         // Matrice 100 broadcasts only flight status
         status = vehicle->broadcast->getStatus();
