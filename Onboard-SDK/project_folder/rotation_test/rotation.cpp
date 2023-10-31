713b33089b8b541647752085c0be89427bb78a4d
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-10-31 14:37:44 +0100

    Restructured code

--- Onboard-SDK/project_folder/rotation_test/rotation.cpp
+++ Onboard-SDK/project_folder/rotation_test/rotation.cpp
@@ -123,7 +123,7 @@ void getRotation(Vehicle* vehicle) {
 
         //std::cout << "\t magnet.x: " << magnet.x << "\n";
         //std::cout << "\t magnet.y: " << magnet.y << "\n";
-        std::cout << "\t D meas: " << degree << ", Changed: " << degree - degStart << " :)\n";
+        //std::cout << "\t D meas: " << degree << ", Changed: " << degree - degStart << " :)\n";
         std::cout << time << "," << degree << "\n";
         /* else {
             std::cout << "\t magnet.y = 0"
