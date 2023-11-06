8da776de83598cd4df37fffc28abf882a43fd4f2
Lukas-Fasken <56631858+Lukas-Fasken@users.noreply.github.com>
2023-11-06 10:03:04 +0100

    changed to ubunto 1404

--- DigitalSignal/Programme/spi/spi.cpp
+++ DigitalSignal/Programme/spi/spi.cpp
@@ -1,20 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
-#include <errno.h>
-
-
-
 #include <iostream>
-#include "spi.h"
+#include <cstdint>
+#include <sys/mman.h>
+#include <fcntl.h>
 
-#define SPI_BASEADDR 0x43d00000
+// Define the base address of the register you want to read (replace with actual address)
+#define REGISTER_BASE_ADDRESS 0x43d00000
 
+int main() {
+    int fd = open("/dev/mem", O_RDWR | O_SYNC);
+    if (fd == -1) {
+        std::cerr << "Failed to open /dev/mem" << std::endl;
+        return 1;
+    }
 
+    // Map the register to a memory address
+    volatile uint32_t* register_address = (volatile uint32_t*)mmap(NULL, sizeof(uint32_t), PROT_READ, MAP_SHARED, fd, REGISTER_BASE_ADDRESS);
+    if (register_address == MAP_FAILED) {
+        std::cerr << "Memory mapping failed" << std::endl;
+        close(fd);
+        return 1;
+    }
 
-void spiMode(int a){
-volatile uint32_t* BIT_LEN=reinterpret_cast<volatile uint32_t*>(SPI_BASEADDR);
-*BIT_LEN |=(1<<4);
+    // Read and print the contents of the register
+    uint32_t register_value = *register_address;
+    std::cout << "Register Value: 0x" << std::hex << register_value << std::dec << std::endl;
 
+    // Unmap the memory
+    munmap(register_address, sizeof(uint32_t));
+    close(fd);
+
+    return 0;
 }
