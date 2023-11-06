0b7071f7ca0c8edd8de004a719c5cb0677e67c0c
Lukas-Fasken <56631858+Lukas-Fasken@users.noreply.github.com>
2023-11-06 09:52:40 +0100

    m

--- DigitalSignal/Programme/spi/main.cpp
+++ DigitalSignal/Programme/spi/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 #define SPI_BASEADDR 0x43d00000
 
 int main(){
-
+/*
 #define SPI_COMMAND1_BIT_LEN 0x04 //define the length of the message -1
 *(volatile uint32_t *)SPI_COMMAND1_BIT_LEN = 14;
 
@@ -16,6 +16,7 @@ int main(){
 
 #define SPI_DMA_CTL_DMA 0x31 //enable DMA, must be set last
 *(volatile uint32_t *)SPI_DMA_CTL_DMA = 1;
+*/
 
 volatile uint32_t* BIT_LEN=reinterpret_cast<volatile uint32_t*>(SPI_BASEADDR);
 *BIT_LEN |=(1<<4);
