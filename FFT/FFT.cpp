67b400cb30a18277a39d0c3d6b216180ed3ee5dc
chrell99 <129071779+chrell99@users.noreply.github.com>
2023-11-10 13:55:22 +0100

    Added function to convert string vector to double vector

--- FFT/FFT.cpp
+++ FFT/FFT.cpp
@@ -6,8 +6,20 @@
 #include <vector>
 #include <sstream>
 #include <unistd.h>
+#include <cmath>
 
 using namespace std;
+
+//only used for trouble shooting simply to check if file name / location is correct
+void chk_file_name(string file_name) {
+    ifstream data_file(file_name); //File location from location of code file
+    if (data_file.is_open()) {     //Checks if file was opened
+        cout << "File can be opened" << endl;
+    } else { //If file couldn't be opened print out error
+        cout << "Unable to open file" << endl;
+    }
+    data_file.close();
+}
 
 //Open csv file and save each value as string in vector textvalue
 void get_string_from_file_csv(string file_name, vector<string> *text_value){
@@ -33,23 +45,28 @@ void get_string_from_file_csv(string file_name, vector<string> *text_value){
     data_file.close();
 }
 
-//only used for trouble shooting simply to check if file name / location is correct
-void chk_file_name(string file_name) {
-    ifstream data_file(file_name); //File location from location of code file
-    if (data_file.is_open()) {     //Checks if file was opened
-        cout << "File can be opened" << endl;
-    } else { //If file couldn't be opened print out error
-        cout << "Unable to open file" << endl;
+//Convert string vector to double vector
+void vec_str_to_vec_double(vector<string> *str, vector<double> *doub){
+    for (const string& str : *str) { //Iterate through the whole string vector
+        try {
+            double value = stod(str);
+            doub->push_back(value);
+        } catch (const invalid_argument& e) { //Catch errors which can be thrown by the stod func
+            cout << "Invalid argument: " << e.what() << endl;
+        } catch (const out_of_range& e) {
+            cout << "Out of range: " << e.what() << endl;
+        }
     }
-    data_file.close();
 }
 
 int main() {
     vector<double> data;
     vector<string> data_string;
     get_string_from_file_csv("Test_Signals/100Hz_4096_signal.csv",&data_string);
-    for (const auto& val : data_string) {
-            std::cout << val << " ";
+    vec_str_to_vec_double(&data_string,&data);
+    for (const double& val : data) {
+        cout << val << " ";
     }
+
     return 0;
 }
