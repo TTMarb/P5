3d81fa0ab16f71be303ad22053cfb218df604b61
chrell99 <129071779+chrell99@users.noreply.github.com>
2023-11-10 13:27:56 +0100

    Added function to read values from .csv files and test .csv file

--- FFT/FFT.cpp
+++ FFT/FFT.cpp
@@ -4,10 +4,36 @@
 #include <stdio.h>
 #include <string>
 #include <vector>
+#include <sstream>
 #include <unistd.h>
 
 using namespace std;
 
+//Open csv file and save each value as string in vector textvalue
+void get_string_from_file_csv(string file_name, vector<string> *text_value){
+
+    string line; // Create string to temp contain value of a line from file
+    ifstream data_file(file_name); //Open file and save to data_file
+    
+    if (!data_file.is_open()) { // Check if the file is opened successfully
+        cerr << "Error opening file!" << endl;
+        return;
+    }
+    
+    while (getline(data_file, line)) { // Save line from data_file to line and repeat for all lines in file
+
+        stringstream ss(line); // Use a stringstream to parse each line
+        string value;  // String to temp contain each value from file
+        //vector<string> values;  // Vector to store individual values
+
+        while (getline(ss, value, ',')) {// Read each value using getline with ',' as the delimiter
+            text_value->push_back(value); // Add the value to the vector -> is the same as (*textvalue).pushback
+        }
+    }
+    data_file.close();
+}
+
+//only used for trouble shooting simply to check if file name / location is correct
 void chk_file_name(string file_name) {
     ifstream data_file(file_name); //File location from location of code file
     if (data_file.is_open()) {     //Checks if file was opened
@@ -19,6 +45,11 @@ void chk_file_name(string file_name) {
 }
 
 int main() {
-    chk_file_name("Test_Signals/100Hz_4096_signal.mat");
+    vector<double> data;
+    vector<string> data_string;
+    get_string_from_file_csv("Test_Signals/100Hz_4096_signal.csv",&data_string);
+    for (const auto& val : data_string) {
+            std::cout << val << " ";
+    }
     return 0;
 }
