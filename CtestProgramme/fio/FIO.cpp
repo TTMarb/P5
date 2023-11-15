770b46ae5cd51c0931b9f03a13730c1b970a20cf
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-11-15 10:20:36 +0100

    Playing with time

--- CtestProgramme/fio/FIO.cpp
+++ CtestProgramme/fio/FIO.cpp
@@ -40,6 +40,6 @@ std::string FIO::prependFolderToFilename(std::string filename) {
     const auto now = std::chrono::system_clock::now();
     const std::time_t t_c = std::chrono::system_clock::to_time_t(now);
     std::cout << std::ctime(&t_c);
-    filename.insert(0, "./" + FIO::folderName + "/"); // insert the folder name in front of the filename
+    filename.insert(0, "./" + FIO::folderName + std::ctime(&t_C) + "/"); // insert the folder name in front of the filename
     return filename;
 }
