6b5014ca383122a3633ca5926b6678b1df863622
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-11-15 08:38:08 +0100

    trying std::string out

--- CtestProgramme/fio/FIO.cpp
+++ CtestProgramme/fio/FIO.cpp
@@ -4,11 +4,11 @@
 FIO::FIO() { std::cout << "Created FIO" << std::endl; }
 
 //Init function, that returns the serial port
-int FIO::createAndOverwriteFile(std::string filename, std::ios::app) {
-    std::ofstream outputFile(filename.c_str()); // create a new output file or overwrite an existing one
-    if (outputFile.is_open()) {                 // check if the file was opened successfully
-        outputFile << "Hello, world!\n";        // write data to the file
-        outputFile.close();                     // close the file when done
+int FIO::createAndOverwriteFile(std::string filename) {
+    std::ofstream outputFile(filename.c_str(), std::ios::app); // create a new output file or overwrite an existing one
+    if (outputFile.is_open()) {                                // check if the file was opened successfully
+        outputFile << "Hello, world!\n";                       // write data to the file
+        outputFile.close();                                    // close the file when done
         std::cout << "Data was written to output.txt\n";
     } else {
         std::cerr << "Error opening file\n";
