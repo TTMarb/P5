eeda931de5b708df229e063435b05023e7a74643
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-11-15 08:57:51 +0100

    learning about strings

--- CtestProgramme/fio/main.cpp
+++ CtestProgramme/fio/main.cpp
@@ -11,7 +11,7 @@
 int main() {
     FIO fileIO = FIO();
     for (int j = 0; j < 5; j++) {
-        std::string filename = "/outputfolder/test" + std::to_string(j);
+        std::string filename = "./outputfolder/test" + std::to_string(j);
         filename.append(".csv");
         fileIO.createFile(filename);
         for (int i = 0; i < 5; i++) {
