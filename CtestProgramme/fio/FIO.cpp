f653d0aab70345234bfc24d1f50a5dfffff2b611
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-11-15 09:55:18 +0100

    Removed remains of old file

--- CtestProgramme/fio/FIO.cpp
+++ CtestProgramme/fio/FIO.cpp
@@ -36,7 +36,7 @@ int FIO::write2file(int time, float angle) {
     return 0;
 }
 
-std::string fIO::prependFolderToFilename(std::string filename) {
+std::string FIO::prependFolderToFilename(std::string filename) {
     filename.insert(0, "./" + FIO::folderName + "/"); // insert the folder name in front of the filename
     return filename;
 }
