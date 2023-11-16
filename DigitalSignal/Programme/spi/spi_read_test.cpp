189cd0627f706f4f079688e07cf915cad8ee2a03
chrell99 <129071779+chrell99@users.noreply.github.com>
2023-11-16 10:59:14 +0100

    Removed unused function

--- DigitalSignal/Programme/spi/spi_read_test.cpp
+++ DigitalSignal/Programme/spi/spi_read_test.cpp
@@ -37,48 +37,6 @@ void open_and_configure() {
 
 }
 
-void continuousSpiRead(int spi_fd, size_t bufferSize) {
-    // Allocate a buffer for reading data
-    unsigned char* buffer = new unsigned char[bufferSize];
-
-    // Set up a spi_ioc_transfer structure
-    struct spi_ioc_transfer transfer = {
-        .tx_buf = 0,  // No data to transmit
-        .rx_buf = (unsigned long)buffer,
-        .len = bufferSize,
-        .delay_usecs = 0,
-        .speed_hz = 1000000,  // Set the desired speed in Hz
-        .bits_per_word = 8,
-    };
-
-    // Infinite loop for continuous reading
-    while (true) {
-        // Read data from SPI device using ioctl
-        auto start_time = chrono::high_resolution_clock::now();
-        if (ioctl(spi_fd, SPI_IOC_MESSAGE(1), &transfer) == -1) {
-            perror("Error reading from SPI");
-            break;
-        }
-        auto end_time = chrono::high_resolution_clock::now();
-
-        // Process the read data (you may want to replace this with your logic)
-        // For example, print the received bytes:
-        std::cout << "Received " << transfer.len << " bytes: ";
-        auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
-        cout << duration.count() << " microseconds: ";
-        for (size_t i = 0; i < transfer.len; ++i) {
-            std::cout << std::hex << static_cast<int>(buffer[i]) << " ";
-        }
-        std::cout << std::endl;
-
-        // Add any necessary delay or sleep here if needed
-        //usleep(100);  // Sleep for 100 milliseconds (adjust as needed)
-    }
-
-    // Cleanup allocated memory
-    delete[] buffer;
-}
-
 void readSPI(int spi_fd) {
     char buffer[256];
     
@@ -108,7 +66,6 @@ void readSPI(int spi_fd) {
 
 int main(){
     open_and_configure();
-    continuousSpiRead(spi_fd,256);
-    //readSPI(spi_fd);
+    readSPI(spi_fd);
     return 0;
 }
