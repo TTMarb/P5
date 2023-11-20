add780d2016a38325a034a6eb3d6098ef1343582
CKjaer <ChristianKjaerNielsen@gmail.com>
2023-11-20 16:21:23 +0100

    Fixed main

--- antenna_dft/main.c
+++ antenna_dft/main.c
@@ -1,11 +1,11 @@
-
-#include <stdint.h>
+#include "stdint.h"
+#include "stdio.h"
 
 uint8_t runOnce = 0;
 
 int main(void) {
     if (runOnce == 0) {
-        printf("Started antenna_dft");
+        printf("Started antenna_dft\n");
         runOnce = 1;
     }
 }
