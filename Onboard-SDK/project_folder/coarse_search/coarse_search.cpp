571c46796e12c579dac8f04090836c4af3ed2e15
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-11-21 11:57:09 +0100

    Changed capitalization

--- Onboard-SDK/project_folder/coarse_search/coarse_search.cpp
+++ Onboard-SDK/project_folder/coarse_search/coarse_search.cpp
@@ -104,7 +104,7 @@ float32_t QtoDEG(Vehicle* vehicle) {
     double t1 = +2.0 * (quaternion.q1 * quaternion.q2 + quaternion.q0 * quaternion.q3);
     double t0 = -2.0 * (quaternion.q2 * quaternion.q2 + quaternion.q3 * quaternion.q3) + 1.0;
     //180/M_pi is to convert from radians to degrees
-    float32_t angle = atan2(t1, t0) * 180 / M_PI;
+    float32_t angle = (atan2(t1, t0) * 180 / M_PI)-90;
     return angle;
 }
 
