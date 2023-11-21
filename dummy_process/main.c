1378d3eaf9399dd01114ed063026c5d1606540aa
CKjaer <ChristianKjaerNielsen@gmail.com>
2023-11-21 09:39:26 +0100

    Added printf

--- dummy_process/main.c
+++ dummy_process/main.c
@@ -44,8 +44,8 @@ int main(void) {
         exit(EXIT_FAILURE); /* Exit the child process if it fails*/
     } else if (pid > 0) {
         printf("Transceiver parent process running. PID is: %d\n", getpid());
-        exit(EXIT_SUCCESS);
         printf("In else if statement\n");
+        exit(EXIT_SUCCESS);
     }
 
     printf("Continue after spawning child\n");
