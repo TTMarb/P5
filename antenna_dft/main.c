c28f72328b3595d90d0937a31184b07def7de337
CKjaer <ChristianKjaerNielsen@gmail.com>
2023-11-21 09:16:58 +0100

    Edit

--- antenna_dft/main.c
+++ antenna_dft/main.c
@@ -2,7 +2,8 @@
 #include "stdio.h"
 
 int main(void) {
-    for (int i = 0; i < 30; i++) {
+    int i;
+    for (i = 0; i < 30; i++) {
         if (i == 0) {
             printf("Began count!\n");
         }
