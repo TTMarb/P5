598c1ebf104cc8d226edc440fed2e64ed1ab41b7
CKjaer <ChristianKjaerNielsen@gmail.com>
2023-11-22 12:46:54 +0100

    Fix

--- domain_socket/dummy_process/main.c
+++ domain_socket/dummy_process/main.c
@@ -47,8 +47,6 @@ int main(void) {
         printf("Waiting to receive...\n");
 
         rc = recvfrom(client_sock, buf, sizeof(float) * BUFFER_SIZE, 0, (struct sockaddr*)&server_adress, &len);
-        printf("RC is %d\n", rc);
-        /*
         if (rc == -1) {
             if (count == 0) {
                 printf("RECEIVE ERROR: NO SERVER AVAILABLE. WAITING");
@@ -74,7 +72,6 @@ int main(void) {
                 printf("%f\n", buf[i]);
             }
         }
-        */
     }
 
     /* 
