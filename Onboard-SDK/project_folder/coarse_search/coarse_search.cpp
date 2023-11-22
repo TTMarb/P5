8f11de60188df3b4adc0e972ccf25c35e4af05cb
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-11-22 12:01:31 +0100

    A1 - A2 insted of the other one

--- Onboard-SDK/project_folder/coarse_search/coarse_search.cpp
+++ Onboard-SDK/project_folder/coarse_search/coarse_search.cpp
@@ -83,7 +83,7 @@ void tellMeAboutTheData(DJI::OSDK::Vehicle* vehicle){
         std::cout << "\t Signal strength: " << signalStrength << "\n";
         std::cout << "\t A1: " << A1 << "\n";
         std::cout << "\t A2: " << A2 << "\n";
-        float64_t alg = M_PI_4-acos((A1-A2)/sqrt(pow(A1,2)+pow(A2,2)));
+        float64_t alg = M_PI_4-acos(abs(A1-A2)/sqrt(pow(A1,2)+pow(A2,2)));
         std::cout << "\t Alg: " << alg << "\n";
         sleep(2);
     }
