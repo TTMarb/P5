145c64f84f8d73f7d2c11ebcbc5d4e67234f8e2e
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-11-22 12:29:12 +0100

    diff angle can be negative

--- Onboard-SDK/project_folder/coarse_search/coarse_search.cpp
+++ Onboard-SDK/project_folder/coarse_search/coarse_search.cpp
@@ -69,9 +69,6 @@ void tellMeAboutTheData(DJI::OSDK::Vehicle* vehicle){
             targetAngle += 360;
         }
         float64_t diffAngle = targetAngle-droneAngle;
-        if (diffAngle < 0) {
-            diffAngle += 360;
-        }
         float64_t A1 = pow(signalStrength,3)*cos((diffAngle*M_PI/180)-M_PI_4);
         float64_t A2 = pow(signalStrength,3)*cos((diffAngle*M_PI/180)+M_PI_4);
         std::cout << "dX: " << dX << ", dY: " << dY << "\n";
