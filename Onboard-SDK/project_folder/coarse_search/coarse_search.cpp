3e3f815b35947624d4e287f524d96109758ac4b2
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-11-22 13:21:46 +0100

    Added H

--- Onboard-SDK/project_folder/coarse_search/coarse_search.cpp
+++ Onboard-SDK/project_folder/coarse_search/coarse_search.cpp
@@ -80,7 +80,8 @@ void tellMeAboutTheData(DJI::OSDK::Vehicle* vehicle){
         std::cout << "\t Signal strength: " << signalStrength << "\n";
         std::cout << "\t A1: " << A1 << "\n";
         std::cout << "\t A2: " << A2 << "\n";
-        float64_t alg = acos((A1-A2)/sqrt(pow(A1,2)+pow(A2,2)))-M_PI_2;
+        float64_t H = sqrt(pow(A1,2)+pow(A2,2));
+        float64_t alg = acos((A1-A2)/H)-M_PI_2;
         std::cout << "\t Alg: " << alg << "\n";
         sleep(2);
     }
