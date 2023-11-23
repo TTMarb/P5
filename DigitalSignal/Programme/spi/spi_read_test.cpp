e461a22cdef3559104b645e0fad9874a6efb8be6
chrell99 <129071779+chrell99@users.noreply.github.com>
2023-11-23 10:54:44 +0100

    Freq adjust

--- DigitalSignal/Programme/spi/spi_read_test.cpp
+++ DigitalSignal/Programme/spi/spi_read_test.cpp
@@ -19,7 +19,7 @@ void open_and_configure() {
 
     uint8_t spi_mode = SPI_MODE_0; // SPI mode (mode 0, 1, 2, or 3)
     uint8_t bpw = 8;     // Bits per word (8, 16, etc.)
-    uint32_t speed = 100000;     // SPI speed (Hz)
+    uint32_t speed = 3500000;     // SPI speed (Hz)
 
     spi_fd = open(spi_device, O_RDWR); //Opens the the file "spi_device" in function O_RDWR "Read or write". Save file descriptor in spi_fd
     if (spi_fd < 0) {
