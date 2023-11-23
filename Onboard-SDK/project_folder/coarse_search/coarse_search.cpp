a13686bbea7193d1a2241b62e0401592a8100a67
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-11-23 16:56:13 +0100

    More random?

--- Onboard-SDK/project_folder/coarse_search/coarse_search.cpp
+++ Onboard-SDK/project_folder/coarse_search/coarse_search.cpp
@@ -111,15 +111,15 @@ DataFaker::DataFaker(Vehicle* vehicle, int sT, int sR) {
     pos = vehicle->broadcast->getGlobalPosition(); 
     searchRadius = sR;
     sampleTime = sT;
+    int rand;
 
     iY = calcMfromLat(pos);
     iX = calcMfromLon(pos);
     srand((unsigned) time(NULL));
-    int rand1 = (-searchRadius + (rand() % (2*searchRadius)));
-    srand((unsigned) time(NULL));
-    int rand2 = (-searchRadius + (rand() % (2*searchRadius)));
-    tX = rand1;
-    tY = rand2;
+    rand = (-searchRadius + (rand() % (2*searchRadius)));
+    tX = rand;
+    rand = (-searchRadius + (rand() % (2*searchRadius)));
+    tY = rand;
 
     std::cout << "target position calculated: tX = " << tX << ", tY = " << tY << "\n";
     std::cout << "about to enter while loop: \n";
