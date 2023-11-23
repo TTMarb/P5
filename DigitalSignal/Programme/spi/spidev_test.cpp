f3dfba0bc6350276c330fc8705d543992be49fe2
chrell99 <129071779+chrell99@users.noreply.github.com>
2023-11-23 10:45:35 +0100

    Freq adjust

--- DigitalSignal/Programme/spi/spidev_test.cpp
+++ DigitalSignal/Programme/spi/spidev_test.cpp
@@ -32,7 +32,7 @@ static void pabort(const char *s)
 static const char *device = "/dev/spidev3.0";
 static uint8_t mode;
 static uint8_t bits = 8;
-static uint32_t speed = 500000;
+static uint32_t speed = 50000000;
 static uint16_t delay;
 
 static void transfer(int fd)
