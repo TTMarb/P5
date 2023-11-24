c7b061c2efee9d3dfea0c9b726e0fec758996120
chrell99 <129071779+chrell99@users.noreply.github.com>
2023-11-24 14:10:58 +0100

    Change bpw

--- DigitalSignal/Programme/spi/adc_read.cpp
+++ DigitalSignal/Programme/spi/adc_read.cpp
@@ -20,7 +20,7 @@ int spi_fd;
 void open_and_configure() {
 
     uint8_t spi_mode = SPI_MODE_0; // SPI mode (mode 0, 1, 2, or 3)
-    uint8_t bpw = 8;               // Bits per word (8, 16, etc.)
+    uint8_t bpw = 16;               // Bits per word (8, 16, etc.)
     uint32_t speed = 32000000;      // SPI speed (Hz)
 
     spi_fd = open(
