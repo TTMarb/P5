564369621edfd81a5d92e82bb259af5151612aea
chrell99 <129071779+chrell99@users.noreply.github.com>
2023-11-24 10:13:37 +0100

    Deleted unused file and made new main file

---
+++ DigitalSignal/Programme/spi/adc_read.cpp
@@ -0,0 +1,55 @@
+#include <fcntl.h>
+#include <iostream>
+#include <chrono>
+#include <cstring>
+#include <linux/spi/spidev.h> // File that's located on the manifold in /usr/include/
+#include <string>
+#include <sys/ioctl.h> // File that's located on the manifold in /usr/include/
+#include <unistd.h>
+#include <cstdint>
+#include <thread>
+
+using namespace std;
+
+const char *spi_device = "/dev/spidev3.0"; // SPI device file
+int spi_fd;
+
+void open_and_configure() {
+
+    uint8_t spi_mode = SPI_MODE_0; // SPI mode (mode 0, 1, 2, or 3)
+    uint8_t bpw = 8;     // Bits per word (8, 16, etc.)
+    uint32_t speed = 3500000;     // SPI speed (Hz)
+
+    spi_fd = open(spi_device, O_RDWR); //Opens the the file "spi_device" in function O_RDWR "Read or write". Save file descriptor in spi_fd
+    if (spi_fd < 0) {
+        cerr << "Failed to open SPI device." << endl;
+        return;
+    }
+
+    if (ioctl(spi_fd, SPI_IOC_WR_MODE, &spi_mode) == -1 ||
+        ioctl(spi_fd, SPI_IOC_WR_BITS_PER_WORD, &bpw) == -1 ||
+        ioctl(spi_fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed) == -1) 
+    {
+        perror("Error setting SPI parameters");
+        close(spi_fd);
+    }
+
+}
+
+int main(){
+    open_and_configure();
+    uint16_t buffer[1];
+    while (true) {
+        int bytes_read = read(spi_fd, buffer, sizeof(buffer));
+        if (bytes_read < 0) {
+            perror("Error reading from SPI");
+            break;
+        }
+        else if (bytes_read > 0){
+            cout << buffer[0] << endl;
+        }
+
+    }
+    
+    return 0;
+}
