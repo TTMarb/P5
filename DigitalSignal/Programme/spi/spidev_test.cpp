5de7a0cf13cc4f004469e7f309e5e4f13a1aaf37
chrell99 <129071779+chrell99@users.noreply.github.com>
2023-11-27 09:30:50 +0100

    Troubleshooting

--- DigitalSignal/Programme/spi/spidev_test.cpp
+++ DigitalSignal/Programme/spi/spidev_test.cpp
@@ -60,6 +60,10 @@ static void transfer(int fd)
 	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
 	if (ret < 1)
 		pabort("can't send spi message");
+	
+	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+	if (ret < 1)
+		pabort("can't send spi message");
 
 	for (ret = 0; ret < ARRAY_SIZE(tx); ret++) {
 		if (!(ret % 6))
