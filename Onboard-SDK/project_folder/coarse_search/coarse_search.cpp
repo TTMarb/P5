1f4512fe42b338c1399790925ee568e9e2e58254
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-11-28 12:49:30 +0100

    Added vel back

--- Onboard-SDK/project_folder/coarse_search/coarse_search.cpp
+++ Onboard-SDK/project_folder/coarse_search/coarse_search.cpp
@@ -44,7 +44,7 @@ void tellMeAboutTheData(DJI::OSDK::Vehicle* vehicle){
 
     std::cout << "Bout to calculate init position: \n";  
     pos = vehicle->broadcast->getGlobalPosition();
-    PIcontroller yawRate = PIcontroller(2, 1, sampleFrequency);
+    PIcontroller yawRate = PIcontroller(0.545, 0, sampleFrequency);
     PIcontroller vX = PIcontroller(0.05, 0, sampleFrequency);
     PIcontroller vY = PIcontroller(0.05, 0, sampleFrequency);
     DataFaker df = DataFaker(vehicle, 1000, searchRadius);
