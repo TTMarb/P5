f3cbb1019a9ba5f79798612b9a892c4de9b717d2
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-11-28 14:33:27 +0100

    Better distance calculation

--- Onboard-SDK/project_folder/coarse_search_sim/coarse_search.cpp
+++ Onboard-SDK/project_folder/coarse_search_sim/coarse_search.cpp
@@ -68,7 +68,7 @@ void tellMeAboutTheData(DJI::OSDK::Vehicle* vehicle){
         //@TODO: istedet for at tilføje 0.001 til H, skal vi have lavet en if else statement :D
         alg = acos((A1-A2)/(H+0.001))-M_PI_2;
         alg = alg*(180/M_PI);
-        vel = pow((sqrt(2)*searchRadius),3)-H;
+        vel = 1-H;
         yawRate.updatePIController(alg);
         //Calculate velocity in x and y direction
         //Sets velocity and yaw rate  
