df70c1e5ffa5bb02dbe3e28d0e19e99238768389
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-11-28 12:28:22 +0100

    Added vel back

--- Onboard-SDK/project_folder/coarse_search/coarse_search.cpp
+++ Onboard-SDK/project_folder/coarse_search/coarse_search.cpp
@@ -65,7 +65,7 @@ void tellMeAboutTheData(DJI::OSDK::Vehicle* vehicle){
         for (int i = 0; i < sampleFrequency; i++){
             UAVAngle = QtoDEG(vehicle);
             vX.updatePIController(vel*sin(UAVAngle*(M_PI/180)));
-            vY.updatePIController(vel*cos(UAVAngle*(M_PI/180)));
+            vY.updatePIController(0);//vel*cos(UAVAngle*(M_PI/180)));
             vehicle->control->velocityAndYawRateCtrl(vY.PIvalue, vX.PIvalue, 0, yawRate.PIvalue);
             float32_t sampleTimeInMicroSeconds = sampleTimeInSeconds*1000*1000;
             usleep(sampleTimeInMicroSeconds);
