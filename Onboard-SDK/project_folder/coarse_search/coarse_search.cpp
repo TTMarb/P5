992a22d1d1e2e47fa5374860ffd614e2ce4909c4
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-11-28 12:43:46 +0100

    Added vel back

--- Onboard-SDK/project_folder/coarse_search/coarse_search.cpp
+++ Onboard-SDK/project_folder/coarse_search/coarse_search.cpp
@@ -56,7 +56,7 @@ void tellMeAboutTheData(DJI::OSDK::Vehicle* vehicle){
         A2 = df.A2;
         H = sqrt(pow(A1,2)+pow(A2,2));
         //@TODO: istedet for at tilføje 0.001 til H, skal vi have lavet en if else statement :D
-        alg = acos((A1-A2)/(H+0.001))-M_PI_2;
+        alg = acos((A1-A2)/(H+0.001));//-M_PI_2;
         alg = alg*(180/M_PI);
         vel = (sqrt(2)*searchRadius-H);
         yawRate.updatePIController(alg);
