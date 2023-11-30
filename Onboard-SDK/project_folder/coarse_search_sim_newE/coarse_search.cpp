d59d1f3418b7c49e8a130141659b42462a0f20b1
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-11-30 11:09:02 +0100

    Large changes to PI

--- Onboard-SDK/project_folder/coarse_search_sim_newE/coarse_search.cpp
+++ Onboard-SDK/project_folder/coarse_search_sim_newE/coarse_search.cpp
@@ -51,8 +51,8 @@ void tellMeAboutTheData(DJI::OSDK::Vehicle* vehicle){
     std::cout << "Bout to calculate init position: \n";  
     pos = vehicle->broadcast->getGlobalPosition();
     PIcontroller yawRate = PIcontroller(0.1, 0.02, 0);
-    PIcontroller vX = PIcontroller(0.00001, 0, 0.0001);
-    PIcontroller vY = PIcontroller(0.00001, 0, 0.0001);
+    PIcontroller vX = PIcontroller(0.001, 0, 0.0001);
+    PIcontroller vY = PIcontroller(0.001, 0, 0.0001);
 
     std::cout << "X-location 4 transceiver: " << std::endl;
     int xLoc;
