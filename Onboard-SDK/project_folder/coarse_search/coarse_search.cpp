b5270c2dfd0afb54a642e62caa25b78a17afcfdc
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-12-01 12:42:45 +0100

    Created PIcontroller class

--- Onboard-SDK/project_folder/coarse_search/coarse_search.cpp
+++ Onboard-SDK/project_folder/coarse_search/coarse_search.cpp
@@ -78,13 +78,13 @@ void controlVehicle(DJI::OSDK::Vehicle* vehicle, float *A1, float *A2, FIO* file
     float sampleTimeInSeconds = 1/sampleFrequency;
     int maxADCvalue = 4096;
 
-    yawRate.updatePIController(alg);
+    yawRate->updatePIController(alg);
     //Calculate velocity in x and y direction
     //Sets velocity and yaw rate  
     for (int i = 0; i < sampleFrequency; i++){
         UAVAngle = QtoDEG(vehicle);
-        vX.updatePIController(vel*cos(UAVAngle*(M_PI/180)));
-        vY.updatePIController(vel*sin(UAVAngle*(M_PI/180)));
+        vX->updatePIController(vel*cos(UAVAngle*(M_PI/180)));
+        vY->updatePIController(vel*sin(UAVAngle*(M_PI/180)));
         vehicle->control->velocityAndYawRateCtrl(vX->PIvalue, vY->PIvalue, 0, yawRate->PIvalue);
         float sampleTimeInMicroSeconds = sampleTimeInSeconds*1000*1000;
         timecounterMilliseconds += 10;
