94dfb559b83674e84f774a57f0a3fad654011780
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-12-01 12:30:57 +0100

    Created PIcontroller class

--- Onboard-SDK/project_folder/coarse_search/coarse_search.cpp
+++ Onboard-SDK/project_folder/coarse_search/coarse_search.cpp
@@ -253,30 +253,3 @@ double calcMfromLat(Telemetry::GlobalPosition pos){
 double calcMfromLon(Telemetry::GlobalPosition pos){
     return pos.longitude*cos(pos.latitude)*EARTH_RADIUS;
 }
-
-//*************************************************************************************//
-//Implementation of the PIController class - This is an implementation of the controller used in the system
-//*************************************************************************************//
-//Constructor
-PIcontroller::PIcontroller(float Kp_in, float Ki_in, float sampleFrequency){
-    Kp = Kp_in;
-    Ki = Ki_in;
-    sampleTime = 1/sampleFrequency;
-    PIvalue = 0;
-    std::cout << "Setup op PIcontroller done \n \t values: Kp: " << Kp << ", Ki: " << Ki << ", sampleTime: " <<sampleTime << ", init pi: " << PIvalue << std::endl;
-}
-
-/// @brief This function calculates the PI value
-/// @param error The error value that the PI controller should calculate from
-void PIcontroller::updatePIController(float error){
-    PIvalue = Kp;
-    //Stops PI from becomming infinite
-    if(Ki != 0){
-        PIvalue += (sampleTime/Ki)*error;
-    }
-    if(abs(PIvalue) > 10){
-	PIvalue = (PIvalue/abs(PIvalue))*10;
-    }
-    sMinus2 = sMinus1;
-    sMinus1 = error;
-}
--- Onboard-SDK/project_folder/coarse_search/coarse_search.hpp
+++ Onboard-SDK/project_folder/coarse_search/coarse_search.hpp
@@ -64,20 +64,6 @@ float64_t calcMfromLon(Telemetry::GlobalPosition);
 
 void addRandomLocation(float32_t*,float32_t*,int,int);
 
-class PIcontroller {
-  public:
-    float32_t PIvalue = 0.0;
-    PIcontroller(float32_t,float32_t,float32_t); // Constructor
-    void updatePIController(float32_t); // Serial port
-
-  private:
-    float32_t Kp = 0.0;
-    float32_t Ki = 0.0;
-    float32_t sampleTime = 0;
-    float32_t sMinus1 = 0;
-    float32_t sMinus2 = 0;
-};
-
 class DataFaker {
   public:
     DataFaker(); // Constructor
