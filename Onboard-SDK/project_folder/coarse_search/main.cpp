6f3bf28f1a76fd5723337f3ca2b14f4e6e9ae219
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-12-04 11:46:17 +0100

    adjusted yawrate

--- Onboard-SDK/project_folder/coarse_search/main.cpp
+++ Onboard-SDK/project_folder/coarse_search/main.cpp
@@ -55,7 +55,7 @@ int main(int argc, char** argv) {
     A2 = 0;
     sampleFrequency = 100;
     velKp = 0.1;
-    PIcontroller yawRate = PIcontroller(0.75, 0.02, sampleFrequency);
+    PIcontroller yawRate = PIcontroller(0.5, 0.02, sampleFrequency);
     PIcontroller vX = PIcontroller(velKp, 0, sampleFrequency);
     PIcontroller vY = PIcontroller(velKp, 0, sampleFrequency);
 
