057ed39093c9a1cb50ccd009528e3344a12f46f8
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-12-04 17:36:57 +0100

    Changed transceiver location

--- Onboard-SDK/antenna_dft/main.c
+++ Onboard-SDK/antenna_dft/main.c
@@ -61,7 +61,7 @@ int main() {
     int runOnce = 0;
     int calComplete = 0;
     // The transceiver position is set X and Y distance from take-off
-    double tLat = 57.061018;//57.061285;
+    double tLat = 57.061018;//57.061285; 
     double tLon = 10.035955;//10.035939;
     while (1) {
         sleep(1);
