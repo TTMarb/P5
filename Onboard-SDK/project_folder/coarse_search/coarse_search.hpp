16f5ea4dae41e61a00ec421457c01967edb36c2e
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-12-04 09:11:07 +0100

    Added calcH, calcAlg and calcVel

--- Onboard-SDK/project_folder/coarse_search/coarse_search.hpp
+++ Onboard-SDK/project_folder/coarse_search/coarse_search.hpp
@@ -63,7 +63,7 @@ void UAVtakoff(Vehicle*, int);
 void UAVstop(Vehicle*,bool, int);
 
 float calcH(DJI::OSDK::Vehicle*, float*, float*, float*);
-float calcAlg(DJI::OSDK::Vehicle*, float*, float*, float*):
+float calcAlg(DJI::OSDK::Vehicle*, float*, float*, float*);
 float calcVel(DJI::OSDK::Vehicle*, float*, float*, int*, int*);
 
 float32_t getAngle(float32_t, float32_t);
