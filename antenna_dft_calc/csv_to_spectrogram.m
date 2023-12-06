8edd5d4dbc478b617f5817d99eb0b096d7fc2165
chrell99 <129071779+chrell99@users.noreply.github.com>
2023-12-06 13:26:02 +0100

    Compiled cpp file so there's a exe file

--- antenna_dft_calc/csv_to_spectrogram.m
+++ antenna_dft_calc/csv_to_spectrogram.m
@@ -7,7 +7,7 @@ opts.VariableNames = ["Ch1", "Ch2"]; %Specify col names
 opts.VariableTypes = ["single", "single"]; %Specify var type
 
 %Open file and save to two column vectors
-tbl = readtable("C:/Users/chrel/Documents/test_2.csv", opts); %Select file location
+tbl = readtable("C:/Users/chrel/Documents/test9m.csv", opts); %Select file location
 Ch1 = tbl.Ch1; %Set array to contain first column from the table
 Ch2 = tbl.Ch2; %Set array to contain second column from the table
 clear tbl; %Clear table to free up memory
@@ -16,7 +16,7 @@ clear tbl; %Clear table to free up memory
 opts = setvaropts(opts, ["Ch1", "Ch2"], "TrimNonNumeric", true);
 opts = setvaropts(opts, ["Ch1", "Ch2"], "ThousandsSeparator", ",");
 
-tbl = readtable("C:/Users/chrel/Documents/output_test_1.csv", opts);
+tbl = readtable("C:\Users\chrel\Documents\GitHub\P5\antenna_dft_calc\8192_9meter_diller.csv", opts);
 RE = tbl.Ch1; %Set array to contain the real part from the table
 IM = tbl.Ch2; %Set array to contain the imag part from the table
 clear tbl; %Clear table to free up memory
