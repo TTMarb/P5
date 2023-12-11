8a54e59429c3c9bde13db4afe7f960e7098aa3a1
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2023-12-11 18:25:39 +0100

    Changed locations for testing

--- Onboard-SDK/project_folder/coarse_search_short/main.cpp
+++ Onboard-SDK/project_folder/coarse_search_short/main.cpp
@@ -64,7 +64,7 @@ int main(int argc, char** argv, char** envp) {
     A1 = 0;
     A2 = 0;
     sampleFrequency = 100;
-    velKp = 0.25;
+    velKp = 0.1;
     PIcontroller yawRate = PIcontroller(0.15, 0, sampleFrequency); //0.02
     PIcontroller vX = PIcontroller(velKp, 0, sampleFrequency);
     PIcontroller vY = PIcontroller(velKp, 0, sampleFrequency);
